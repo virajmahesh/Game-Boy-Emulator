325c82724b6c42f1276252fc2e5eb0a479824578
Viraj Mahesh <virajmahesh@gmail.com>
2015-12-23 01:04:53 +0400

    Document test_util functions

--- test/test_util.h
+++ test/test_util.h
@@ -7,18 +7,30 @@
 #include <cstdint>
 #include <cstdlib>
 
+/**
+ * Generate a random byte.
+ */
 uint8_t random_byte() {
     return static_cast<uint8_t>(rand());
 }
 
+/**
+ * Generate a random word (2 bytes).
+ */
 uint16_t random_word() {
     return static_cast<uint16_t>(rand());
 }
 
+/*
+ * Get the first (most significant) byte of a word.
+ */
 uint8_t high_byte(uint16_t word) {
     return static_cast<uint8_t>(word >> 8);
 }
 
+/**
+ * Get the second (least significant) byte of a word.
+ */
 uint8_t low_byte(uint16_t word) {
     return static_cast<uint8_t>((word << 8) >> 8);
 }
