f85b27e89aeae3925e2d08e05b4a19d4069adc76
Viraj Mahesh <virajmahesh@gmail.com>
2015-12-24 00:11:50 +0400

    Add more util functions to test_util.h

--- test/test_util.h
+++ test/test_util.h
@@ -22,6 +22,25 @@ uint16_t random_word() {
 }
 
 /*
+ * Generate a random word in the range [a, b)
+ */
+uint16_t random_word(uint16_t a, uint16_t b) {
+    return static_cast<uint16_t>((rand() % (b - a)) + a);
+}
+
+/*
+ * Return a random byte array of a particular size. Remember to deallocate
+ * the byte array later.
+ */
+uint8_t* random_byte_array(uint32_t size) {
+    uint8_t *byte_array = new uint8_t[size];
+    for (uint32_t i = 0; i < size; i++) {
+        byte_array[i] = random_byte();
+    }
+    return byte_array;
+}
+
+/*
  * Get the first (most significant) byte of a word.
  */
 uint8_t high_byte(uint16_t word) {
