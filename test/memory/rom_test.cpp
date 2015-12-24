b106ca1615b4eac2f0de8dfc3ba3bfbe856ba2d7
Viraj Mahesh <virajmahesh@gmail.com>
2015-12-24 09:24:05 +0400

    Add test cases to ensure that ROM bank 0 is accessible

--- test/memory/rom_test.cpp
+++ test/memory/rom_test.cpp
@@ -43,6 +43,55 @@ TEST(MBC1_16_8_ROM_Test, Load_Byte_From_ROM_Bank_0) {
 }
 
 /*
+ * Test that Bank 0 is accessible in cartridges with an MBC 1
+ * controller in 4/32 mode.
+ */
+TEST(MBC1_4_32_ROM_Test, Load_Byte_From_ROM_Bank_0) {
+    uint32_t rom_size = Cartridge::BANK_SIZE;
+    Cartridge rom = Cartridge(MBC1_4_32, random_byte_array(rom_size), rom_size, 0);
+
+    uint16_t addr = random_word(0x0000, 0x4000);
+
+    EXPECT_EQ(rom.access_rom_data(addr), rom.load_byte_rom(addr));
+}
+
+/*
+ * Test that Bank 0 is accessible in cartridges with an MBC 2 controller.
+ */
+TEST(MBC2_ROM_Test, Load_Byte_From_ROM_Bank_0) {
+    uint32_t rom_size = Cartridge::BANK_SIZE;
+    Cartridge rom = Cartridge(MBC2, random_byte_array(rom_size), rom_size, 0);
+
+    uint16_t addr = random_word(0x0000, 0x4000);
+
+    EXPECT_EQ(rom.access_rom_data(addr), rom.load_byte_rom(addr));
+}
+
+/*
+ * Test that Bank 0 is accessible in cartridges with an MBC 3 controller.
+ */
+TEST(MBC3_ROM_Test, Load_Byte_From_ROM_Bank_0) {
+    uint32_t rom_size = Cartridge::BANK_SIZE;
+    Cartridge rom = Cartridge(MBC3, random_byte_array(rom_size), rom_size, 0);
+
+    uint16_t addr = random_word(0x0000, 0x4000);
+
+    EXPECT_EQ(rom.access_rom_data(addr), rom.load_byte_rom(addr));
+}
+
+/*
+ * Test that Bank 0 is accessible in cartridges with an MBC 5 controller.
+ */
+TEST(MBC5_ROM_Test, Load_Byte_From_ROM_Bank_0) {
+    uint32_t rom_size = Cartridge::BANK_SIZE;
+    Cartridge rom = Cartridge(MBC5, random_byte_array(rom_size), rom_size, 0);
+
+    uint16_t addr = random_word(0x0000, 0x4000);
+
+    EXPECT_EQ(rom.access_rom_data(addr), rom.load_byte_rom(addr));
+}
+
+/*
  * Test that a cartridge with an MBC 1 controller in 16/8 mode can switch all
  * banks into the 0x4000 - 0x7FFF space. Also tests that the 0x00, 0x20, 0x40
  * and 0x60 banks are inaccessible.
@@ -161,7 +210,7 @@ TEST(MBC5_ROM_TEST, Bank_Switching) {
             uint8_t data = rom.access_rom_data(rom_addr(bank, addr));
 
             EXPECT_EQ(data, rom.load_byte_rom(addr));
-            EXPECT_EQ(bank, rom.get_rom_bank()) << hex << (int)i << " " << hex << (int)j;
+            EXPECT_EQ(bank, rom.get_rom_bank());
         }
     }
 }
